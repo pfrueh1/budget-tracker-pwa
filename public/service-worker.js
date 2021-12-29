93b54114a97837506a3ae8e82a363630dc7af0f9
pfrueh1 <fruehpaul@gmail.com>
2021-12-29 12:44:56 -0500

    typo

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,15 +6,7 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./css/style.css",
   "./js/index.js",
-  "./js/idb.js",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png",
+  "./js/idb.js"
 ];
 
 self.addEventListener('install', function (e) {
@@ -57,9 +49,6 @@ self.addEventListener('fetch', function (e) {
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   )
 })
